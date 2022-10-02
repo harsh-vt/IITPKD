7ee86d3871bbf57ce4addaa7477add897e6c2076
Harshvardhan <harshvt247@gmail.com>
2022-10-01 22:30:05 -0500

    Removed keys

--- main.py
+++ main.py
@@ -177,7 +177,7 @@ th     { background:#eee; }
                     <div id="map">
                     </div>
                     <script
-                         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAEX5N6BW6mUGgnor0EBcYwf52twggIs8k" type="text/javascript">
+                         src="https://maps.googleapis.com/maps/api/js?key=KEY" type="text/javascript">
                     </script>
                     <div class = "second_col_legend"><table>
 <tr>
@@ -544,7 +544,7 @@ th     { background:#eee; }
                     <div id="map">
                     </div>
                     <script
-                         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAEX5N6BW6mUGgnor0EBcYwf52twggIs8k" type="text/javascript">
+                         src="https://maps.googleapis.com/maps/api/js?key=KEY" type="text/javascript">
                     </script>
                     <div class = "second_col_legend"><table>
 <tr>
@@ -969,7 +969,7 @@ th     { background:#eee; }
                     <div id="map">
                     </div>
                     <script
-                         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAEX5N6BW6mUGgnor0EBcYwf52twggIs8k" type="text/javascript">
+                         src="https://maps.googleapis.com/maps/api/js?key=KEY" type="text/javascript">
                     </script>
                     <div class = "second_col_legend"><table>
 <tr>
@@ -1411,7 +1411,7 @@ th     { background:#eee; }
                     <div id="sidebar">
                     </div>
                     <script
-                         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAEX5N6BW6mUGgnor0EBcYwf52twggIs8k&callback=initDirMap&v=weekly" type="text/javascript">
+                         src="https://maps.googleapis.com/maps/api/js?key=KEY&callback=initDirMap&v=weekly" type="text/javascript">
                     </script>
                     </div>
                     <div class = "second_col_legend"><table>
